177e22c96b5045703ba4ad204992da050de036c4
zjedzonyy <artompor@gmail.com>
2025-07-10 16:46:29 +0200

    fix(spinner): fix showSpinner import

--- src/components/auth/RequireAuth.jsx
+++ src/components/auth/RequireAuth.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "./index";
 
